0844279bca6603d22308f0559756cb07cde91143
Bayyr Oorjak <the.bayyr.oorjak@gmail.com>
2024-07-12 18:03:21 +0300

    chore: setup prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import config from "eslint-config-standard";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  ...[].concat(config),
+  ...[].concat(config, eslintConfigPrettier),
 ];
