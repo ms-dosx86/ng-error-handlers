168264fdbb250b690f78fe6fb2f142e0a74a0038
Bayyr Oorjak <the.bayyr.oorjak@gmail.com>
2024-07-13 11:33:13 +0300

    fix: sync package.json versions

--- projects/ng-error-handlers/sync-versions.js
+++ projects/ng-error-handlers/sync-versions.js
@@ -1,12 +1,8 @@
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv)).argv
-const fs = require('fs');
-const path = require('path');
+const json = require('json-update');
 
-if (argv.version) {
-    const packageJson = fs.readFileSync(path.resolve('./package.json'), 'utf-8');
-    const data = JSON.parse(packageJson);
-    data.version = argv.version;
-    fs.writeFileSync(path.resolve('./package.json'), JSON.stringify(data), 'utf-8');
-}
+const version = argv.v;
+
+json.update('package.json', { version })
